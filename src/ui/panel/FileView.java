ca16ae8a8833bd4bc2bd29b92749ea359ff5448c
setenfa <yuanmingk7@outlook.com>
2024-10-24 13:16:27 +0800

    新增事件转发机制并优化代码

--- src/ui/panel/FileView.java
+++ src/ui/panel/FileView.java
@@ -1,5 +1,7 @@
 package ui.panel;
 
+import event.PathChangeEvent;
+import event.PathChangeListener;
 import ui.windows.FileViewFlame;
 import util.FAT;
 import util.File;
@@ -9,6 +11,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.ArrayList;
 import java.util.List;
 
 import static res.GlobalResources.FILESYS;
@@ -18,17 +21,18 @@ public class FileView {
     private JPanel fileViewPane;
     private JPopupMenu jPopupMenu;
     private JPopupMenu panelMenu;
+    private JScrollPane scrollPane;
+    private List<PathChangeListener> listeners;
 
     public FileView(JPopupMenu jPopupMenu) {
         this.jPopupMenu = jPopupMenu;
         initFileViewPane();
         initPanelMenu();
-
+        listeners = new ArrayList<>();
     }
 
     public void initFileViewPane() {
         fileViewPane = new JPanel();
-
         fileViewPane.setLayout(new FlowLayout(FlowLayout.LEFT, 10, 10));
         fileViewPane.addMouseListener(new MouseAdapter() {
             @Override
@@ -38,16 +42,17 @@ public class FileView {
                 }
             }
         });
-
+        scrollPane = new JScrollPane(fileViewPane);
+        scrollPane.setPreferredSize(new Dimension(800, 600));
     }
 
-    public JPanel addFileOnPanel(List<FAT> files) {
-        JPanel panel = new JPanel();
+    public void addFileOrFolderOnPanel(List<FAT> files) {
         JLabel label;
         for (FAT fat : files) {
             if (fat != null) {
                 if (fat.getObject() instanceof File file) {
                     label = new JLabel(file.getFileName());
+                    System.out.println(file.getFileName());
                 } else {
                     Folder folder = (Folder) fat.getObject();
                     label = new JLabel(folder.getFolderName());
@@ -58,9 +63,12 @@ public class FileView {
                     public void mouseClicked(MouseEvent e) {
                         if (e.getClickCount() == 2) {
                             if (fat.getObject() instanceof Folder folder) {
-                                String path = folder.getPath() + "\\" + folder.getFolderName();
+                                String newPath = folder.getPath() + "\\" + folder.getFolderName();
+                                TREE.setCurrentNode(TREE.getChildNodeByPathRecursive(TREE.getCurrentNode(), newPath));
+                                // 更新路径栏
+                                firePathChangeEvent(newPath);
                                 fileViewPane.removeAll();
-                                addFileOnPanel(FILESYS.getFilesAndFolders(path));
+                                addFileOrFolderOnPanel(FILESYS.getFilesAndFolders(newPath));
                                 fileViewPane.updateUI();
                                 // 更新ui
                             } else {
@@ -75,6 +83,7 @@ public class FileView {
                                 }
                             }
                         } else if (e.getButton() == MouseEvent.BUTTON3) {
+                            jPopupMenu.removeAll();
                             JMenuItem delete = getDeleteItem(fat);
                             JMenuItem rename = getRenameItem(fat);
                             jPopupMenu.add(delete);
@@ -86,7 +95,6 @@ public class FileView {
                 this.fileViewPane.add(label);
             }
         }
-        return panel;
     }
 
     private JMenuItem getDeleteItem(FAT fat) {
@@ -95,15 +103,19 @@ public class FileView {
             if (fat.getObject() instanceof File) {
                 FILESYS.delete(fat);
             } else {
-                if (!FILESYS.getFilesAndFolders(((Folder) fat.getObject()).getPath()).isEmpty()) {
+                if (!FILESYS.getFilesAndFolders(((Folder) fat.getObject()).getPath() + "\\" + ((Folder) fat.getObject()).getFolderName()).isEmpty()) {
                     JOptionPane.showMessageDialog(null, "文件夹不为空，无法删除");
                     return;
                 } else {
+                    Folder folder = (Folder) fat.getObject();
+                    String path = folder.getPath() + "\\" + folder.getFolderName();
+                    TREE.deleteNode(TREE.getCurrentNode(), path);
                     FILESYS.delete(fat);
                 }
             }
             fileViewPane.removeAll();
-            addFileOnPanel(TREE.getCurrentNode().getUserObject() instanceof Folder folder ? FILESYS.getFilesAndFolders(folder.getPath()) : FILESYS.getFilesAndFolders(""));
+            Folder folder = (Folder) TREE.getCurrentNode().getUserObject();
+            addFileOrFolderOnPanel(FILESYS.getFilesAndFolders(folder.getPath()));
             fileViewPane.updateUI();
         });
         return delete;
@@ -112,19 +124,30 @@ public class FileView {
     private JMenuItem getRenameItem(FAT fat) {
         JMenuItem rename = new JMenuItem("重命名");
         rename.addActionListener(e -> {
-            if (fat.getObject() instanceof File) {
+            if (fat.getObject() instanceof File file) {
                 String fileName = JOptionPane.showInputDialog("请输入新的文件名");
                 if (fileName != null) {
-                    ((File) fat.getObject()).setFileName(fileName);
+                    if (FILESYS.isFileOrFolderExist(file.getPath(), fileName)) {
+                        JOptionPane.showMessageDialog(null, "文件已存在");
+                        return;
+                    } else {
+                        file.setFileName(fileName);
+                    }
                 }
             } else {
                 String folderName = JOptionPane.showInputDialog("请输入新的文件夹名");
                 if (folderName != null) {
-                    ((Folder) fat.getObject()).setFolderName(folderName);
+                    if (FILESYS.isFileOrFolderExist(((Folder) fat.getObject()).getPath(), folderName)) {
+                        JOptionPane.showMessageDialog(null, "文件夹已存在");
+                        return;
+                    } else {
+                        ((Folder) fat.getObject()).setFolderName(folderName);
+                    }
                 }
             }
             fileViewPane.removeAll();
-            addFileOnPanel(TREE.getCurrentNode().getUserObject() instanceof Folder folder ? FILESYS.getFilesAndFolders(folder.getPath()) : FILESYS.getFilesAndFolders(""));
+            Folder folder = (Folder) TREE.getCurrentNode().getUserObject();
+            addFileOrFolderOnPanel(FILESYS.getFilesAndFolders(folder.getPath()));
             fileViewPane.updateUI();
         });
         return rename;
@@ -137,22 +160,53 @@ public class FileView {
         newFile.addActionListener(e -> {
             String fileName = JOptionPane.showInputDialog("请输入文件名");
             if (fileName != null) {
-                FILESYS.createFile(fileName, "1");
-                fileViewPane.removeAll();
-                addFileOnPanel(TREE.getCurrentNode().getUserObject() instanceof Folder folder ? FILESYS.getFilesAndFolders(folder.getPath()) : FILESYS.getFilesAndFolders(""));
-                fileViewPane.updateUI();
+                if (!FILESYS.createFile(fileName, "1")) {
+                    JOptionPane.showMessageDialog(null, "磁盘已满或文件已存在");
+                } else {
+                    fileViewPane.removeAll();
+                    Folder folder = (Folder) TREE.getCurrentNode().getUserObject();
+                    addFileOrFolderOnPanel(FILESYS.getFilesAndFolders(folder.getPath()));
+                    fileViewPane.updateUI();
+                }
             }
         });
         newFolder.addActionListener(e -> {
             String folderName = JOptionPane.showInputDialog("请输入文件夹名");
             if (folderName != null) {
-                FILESYS.createFolder(folderName);
-                fileViewPane.removeAll();
-                addFileOnPanel(TREE.getCurrentNode().getUserObject() instanceof Folder folder ? FILESYS.getFilesAndFolders(folder.getPath()) : FILESYS.getFilesAndFolders(""));
-                fileViewPane.updateUI();
+                if (!FILESYS.createFolder(folderName)) {
+                    JOptionPane.showMessageDialog(null, "磁盘已满或文件夹已存在");
+                } else {
+                    fileViewPane.removeAll();
+                    Folder folder = (Folder) TREE.getCurrentNode().getUserObject();
+                    addFileOrFolderOnPanel(FILESYS.getFilesAndFolders(folder.getPath()));
+                    fileViewPane.updateUI();
+                }
             }
         });
         panelMenu.add(newFile);
         panelMenu.add(newFolder);
+    }
+
+    public JPanel getFileViewPane() {
+        return fileViewPane;
+    }
+
+    public JScrollPane getScrollPane() {
+        return scrollPane;
+    }
+
+    public void addPathChangeListener(PathChangeListener listener) {
+        listeners.add(listener);
+    }
+
+    public void removePathChangeListener(PathChangeListener listener) {
+        listeners.remove(listener);
+    }
+
+    private void firePathChangeEvent(String newPath) {
+        PathChangeEvent event = new PathChangeEvent(this, newPath);
+        for (PathChangeListener listener : listeners) {
+            listener.pathChanged(event);
+        }
     }
 }

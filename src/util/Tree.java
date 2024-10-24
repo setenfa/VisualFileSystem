b50e4555caa4d8cdfc1449dee04538323c05bfda
setenfa <yuanmingk7@outlook.com>
2024-10-24 13:12:51 +0800

    更新删除节点方法，以便删除文件夹时更新树

--- src/util/Tree.java
+++ src/util/Tree.java
@@ -1,5 +1,6 @@
 package util;
 
+import javax.swing.*;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeModel;
 
@@ -13,7 +14,7 @@ public class Tree {
     }
 
     private void initialTree() {
-        Folder root = new Folder("C:");
+        Folder root = new Folder("C", 0, "C:");
         DefaultMutableTreeNode rootNode = new DefaultMutableTreeNode(root);
         this.treeModel = new DefaultTreeModel(rootNode);
         this.currentNode = rootNode;
@@ -23,11 +24,37 @@ public class Tree {
     public void addNode(DefaultMutableTreeNode root, String nodeName) {
         Folder folder = (Folder) root.getUserObject();
         Folder newFolder = new Folder(nodeName);
-        newFolder.setPath(folder.getPath());
-        newFolder.setDiskNum(FILESYS.getFreeBlock());
+        newFolder.setPath(folder.getPath() + "\\" + nodeName);
         DefaultMutableTreeNode newNode = new DefaultMutableTreeNode(newFolder);
         root.add(newNode);
         treeModel.reload(root);
+    }
+
+    // 删除文件夹时调用，更新树
+    public void deleteNode(DefaultMutableTreeNode currentNode, String path) {
+        DefaultMutableTreeNode searchNode = getChildNodeByPathRecursive(currentNode, path);
+        if (searchNode == null) {
+            JOptionPane.showMessageDialog(null, "文件夹不存在");
+            return;
+        } else {
+            DefaultMutableTreeNode parent = (DefaultMutableTreeNode) searchNode.getParent();
+            parent.remove(searchNode);
+            treeModel.reload(parent);
+        }
+    }
+
+    public DefaultMutableTreeNode getChildNodeByPathRecursive(DefaultMutableTreeNode root, String path) {
+        if (root.getChildCount() == 0) {
+            return null;
+        }
+        for (int i = 0; i < root.getChildCount(); i++) {
+            DefaultMutableTreeNode child = (DefaultMutableTreeNode) root.getChildAt(i);
+            Folder folder = (Folder) child.getUserObject();
+            if (folder.getPath().equals(path)) {
+                return child;
+            }
+        }
+        return null;
     }
 
     public DefaultMutableTreeNode getCurrentNode() {

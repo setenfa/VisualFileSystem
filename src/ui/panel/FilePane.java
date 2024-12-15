a3d44fedef34a4cbd35af1cca7617affb8918ec4
setenfa <yuanmingk7@outlook.com>
2024-12-15 16:02:27 +0800

    修改目录树项的外观

--- src/ui/panel/FilePane.java
+++ src/ui/panel/FilePane.java
@@ -1,9 +1,15 @@
 package ui.panel;
 
 
+import util.Folder;
+
 import javax.swing.*;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeCellRenderer;
 import javax.swing.tree.DefaultTreeModel;
 import java.awt.*;
+
+import static res.GlobalResources.FOLDER_ICON_ORIGINAL;
 
 public class FilePane {
     private DefaultTreeModel tree;
@@ -17,6 +23,21 @@ public class FilePane {
     }
 
     public void initFileTreePane() {
+        this.directoryTree.setCellRenderer(new DefaultTreeCellRenderer() {
+            @Override
+            public Component getTreeCellRendererComponent(JTree tree, Object value, boolean sel, boolean expanded,
+                                                          boolean leaf, int row, boolean hasFocus) {
+                JLabel label = (JLabel) super.getTreeCellRendererComponent(tree, value, sel, expanded, leaf, row, hasFocus);
+                DefaultMutableTreeNode node = (DefaultMutableTreeNode) value;
+                Folder folderNode = (Folder) node.getUserObject();
+                if (folderNode == null) {
+                    return label;
+                }
+                label.setIcon(FOLDER_ICON_ORIGINAL);
+                return label;
+            }
+        });
+        this.directoryTree.setShowsRootHandles(true);
         scrollPane = new JScrollPane(directoryTree);
         scrollPane.setPreferredSize(new Dimension(200, 400));
     }
